70901494992faf9b468675e4fe137e19b9239dec
Christopher Barrett <cjbarrettaz@gmail.com>
2026-05-13 20:02:57 +0200

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+import rootRouter from "./routes/root.js";
+import healthRouter from "./routes/health.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(rootRouter);
+app.use(healthRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
