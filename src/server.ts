8b8471bafec960c4891a95fe1e70e3cc6debcf5a
Christopher Barrett <cjbarrettaz@gmail.com>
2026-05-13 20:18:30 +0200

    fixed the project that I intentionally broke

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(rootRouter);
-app.use(healthRouter);/
+app.use(healthRouter);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
