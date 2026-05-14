5c9de5e8d3adbbf5c24690081be1a70c3d2df121
Christopher Barrett <cjbarrettaz@gmail.com>
2026-05-14 09:10:01 +0200

    updated docker build and push commands in staging.yml

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get("/", (_req, res) => {
     res.json({
-        message: "Hello from Express + TypeScript!!!!! w00t!!!!"
+        message: "w00t!!!!"
     });
 });
 
