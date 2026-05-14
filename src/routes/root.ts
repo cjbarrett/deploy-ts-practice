33f0df95639e304c8974ff267154be5c82091ff2
Christopher Barrett <cjbarrettaz@gmail.com>
2026-05-14 06:43:37 +0200

    updated deploy.yml

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get("/", (_req, res) => {
     res.json({
-        message: "w00t! Hello from Express + TypeScript!!! Testing merge to main."
+        message: "w00t! Hello from Express + TypeScript!!!! Testing merge to main."
     });
 });
 
