fdd9de4a3b05aede506b8e2dfb59ff394d768fb9
Christopher Barrett <cjbarrettaz@gmail.com>
2026-05-14 10:01:51 +0200

    testing deploy to main

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get("/", (_req, res) => {
     res.json({
-        message: "testing deploy to Render... w00t!!!!!!!!"
+        message: "testing deploy to Render..."
     });
 });
 
