df283833907faa44d5e15ee2c9e315b59f5a9eaa
cjbarrett <50974216+cjbarrett@users.noreply.github.com>
2026-05-14 09:50:32 +0200

    Staging (#3)
    * testing deployment to Render on staging branch
    
    * setting up to test deployment when merging staging with main
    
    * .
    
    * added Render secret for staging.yml
    
    * test deployment to Render for staging branch
    
    * test deployment to Render for staging branch
    
    * updated staging.yml
    
    * updated deploy.yml
    
    * updated deploy.yml
    
    * updated deploy.yml
    
    * updated deploy.yml
    
    * updated deploy.yml
    
    * updated deploy.yml
    
    * updated root.ts
    
    * updated route in root.ts
    
    * updated docker build and push commands in staging.yml
    
    * updated docker build and push commands in staging.yml
    
    * updated docker build and push commands in staging.yml
    
    * updated docker build and push commands in staging.yml
    
    * updated docker build and push commands in staging.yml
    
    * renamed GitHub Actions-related yml files
    
    * renamed GitHub Actions-related yml files
    
    * updated deploy-prod.yml
    
    * testing the deploy via PR thing again

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get("/", (_req, res) => {
     res.json({
-        message: "testing deploy to Render... w00t!!!!"
+        message: "going to try this PR thing again... w00t!!!!"
     });
 });
 
