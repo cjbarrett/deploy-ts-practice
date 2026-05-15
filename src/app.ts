6e649caa9d83143d34b299d792f63b59ea29b7e4
Christopher Barrett <cjbarrettaz@gmail.com>
2026-05-15 20:35:20 +0200

    refactored server.ts and created app.js

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+import rootRouter from "./routes/root.js";
+import healthRouter from "./routes/health.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(rootRouter);
+app.use(healthRouter);
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,6 @@
-import express from "express";
-
-import rootRouter from "./routes/root.js";
-import healthRouter from "./routes/health.js";
-
-const app = express();
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.use(rootRouter);
-app.use(healthRouter);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
