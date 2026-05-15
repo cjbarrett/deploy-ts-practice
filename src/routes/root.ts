792dd30d2dad55552d8b41a61265bfbf6e84d49a
cjbarrett <50974216+cjbarrett@users.noreply.github.com>
2026-05-15 20:02:55 +0200

    Staging (#17)
    * recreated teh staging branch
    
    * implemented eslint as part of the build process (#8)
    
    * Feature/add linting (#9)
    
    * implemented eslint as part of the build process
    
    * updated deploy-staging.yml to optimize deployment to Render
    
    * Feature/add linting (#10)
    
    * implemented eslint as part of the build process
    
    * updated deploy-staging.yml to optimize deployment to Render
    
    * updated npm run lint with --max-warnings=0 flag
    
    * Feature/test code promotion (#15)
    
    * Staging (#4)
    
    * testing deployment to Render on staging branch
    
    * setting up to test deployment when merging staging with main
    
    * .
    
    * added Render secret for staging.yml
    
    * test deployment to Render for staging branch
    
    * test deployment to Render for staging branch
    
    * updated staging.yml
    
    * updated deploy.yml
    
    * updated deploy.yml
    
    * updated deploy.yml
    
    * updated deploy.yml
    
    * updated deploy.yml
    
    * updated deploy.yml
    
    * updated root.ts
    
    * updated route in root.ts
    
    * updated docker build and push commands in staging.yml
    
    * updated docker build and push commands in staging.yml
    
    * updated docker build and push commands in staging.yml
    
    * updated docker build and push commands in staging.yml
    
    * updated docker build and push commands in staging.yml
    
    * renamed GitHub Actions-related yml files
    
    * renamed GitHub Actions-related yml files
    
    * updated deploy-prod.yml
    
    * testing the deploy via PR thing again
    
    * recreated teh staging branch (#6)
    
    * Staging (#11)
    
    * recreated teh staging branch
    
    * implemented eslint as part of the build process (#8)
    
    * Feature/add linting (#9)
    
    * implemented eslint as part of the build process
    
    * updated deploy-staging.yml to optimize deployment to Render
    
    * Feature/add linting (#10)
    
    * implemented eslint as part of the build process
    
    * updated deploy-staging.yml to optimize deployment to Render
    
    * updated npm run lint with --max-warnings=0 flag
    
    * testing project code promotion through branches

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get("/", (_req, res) => {
     res.json({
-        message: "w00t!!!"
+        message: "w00t!!! CI/CD is coming into place nicely!"
     });
 });
 
